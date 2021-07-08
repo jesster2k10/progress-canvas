f83a630866ac0dd353139779049750f6168de21e
Jesse Onolememen <hey@jesseono.me>
2021-07-08 14:18:25 +0100

    add callback hook

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ export { mountProgressCanvas as mount } from './mount'
 
 window.ProgressCanvas = ProgressCanvas
 window.ProgressCanvas.mount = mountProgressCanvas
+
+if (window.ProgressCanvasCallbacks) {
+  window.ProgressCanvasCallbacks.forEach((callback) => {
+    if (typeof callback === 'function') {
+      callback(window.ProgressCanvas)
+    }
+  })
+}
